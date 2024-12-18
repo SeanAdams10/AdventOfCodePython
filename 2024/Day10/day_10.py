0935fb4e3358770b88b5e6284c4db6ee20f59a48
SeanAdams10 <sean.b.adams@gmail.com>
2024-12-17 19:14:04 -0500

    Day 10 v2

--- 2024/Day10/day_10.py
+++ 2024/Day10/day_10.py
@@ -1,4 +1,5 @@
 from time import perf_counter
+from itertools import product
 
 def get_input_data(year: int, day: int, sample: int = 0) -> list:
     """
@@ -34,16 +35,56 @@ def read_input(data) -> list:
     """
     placeholder for the parser needed for this day's puzzle
     """
-    rules = [(x.split("|")[0].strip(), x.split("|")[1].strip())
-             for x in data if "|" in x]
-    updates = [x.split(",") for x in data if "," in x]
+    grd_values={}
+    for r, d in enumerate(data):
+        for c,v in enumerate(d):
+            if v.isdigit():
+                grd_values[(r,c)] = int(v)
+            else:
+                grd_values[(r,c)] = -1                
+    max_r=len(data)-1
+    max_c= len(data[0])-1
+    
+    grd_neighbours = {}
+    for x in grd_values:
+        combo = product([x], [(-1,0),(+1,0), (0,-1), (0,+1)])
+        grd_neighbours[x] = [(current[0] + delta[0], current[1]+ delta[1]) for current, delta in combo]
+        grd_neighbours[x] = list(filter(lambda x: 0<=x[0]<=max_r and 0<=x[1]<=max_c, grd_neighbours[x]))
 
-    return rules, updates
+    return grd_values, grd_neighbours
 
 
 def main():
-    data = get_input_data(2024, 6, sample=1)
-    read_input(data)
+    data = get_input_data(2024, 10, sample=0)
+    grd_values, grd_neighbours = read_input(data)
+    print(grd_neighbours)
+
+    trail_heads = [cell for cell, value in grd_values.items() if value == 0]
+    result={}
+    for t in trail_heads:
+        current_edge = set([t])
+        for i in range(1,10):
+            current_edge = set([n for e in current_edge for n in grd_neighbours[e] if grd_values[n]==i])
+        result[t] = len(current_edge)
+
+    print(f'Part 1 {sum(result.values())}')
+
+    trail_heads = [cell for cell, value in grd_values.items() if value == 0]
+    result={}
+    for t in trail_heads:
+        current_path = [[t],]
+        for i in range(1,10):
+            next = []
+            for p in current_path:
+                next_nodes = [p + [n] for n in grd_neighbours[p[-1]] if grd_values[n]==i]
+                next.extend(next_nodes)
+            current_path = next
+        result[t] = current_path
+    
+    result_count = [len(x) for x in result.values()]
+    print(f'Part 2 {sum(result_count)}')
+
+
 
 
 if __name__ == "__main__":
